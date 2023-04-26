40e090be7bf2abe9e00885ee58cd9dea335aee40
Thared2203 <tilley.tae@oundleschool.org.uk>
2023-04-26 09:40:00 +0100

    T

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "WINESHOP";
+$dbname = "wineshop";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
